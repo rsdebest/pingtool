f87a3975bc80fbc555c7c93a3aacf3229e1cb8b6
Raoul de Best <raoul@aanzee.nl>
2015-06-11 00:38:56 +0200

    First draft for a working request - needs refactoring

--- app/controllers/pingdom.server.controller.js
+++ app/controllers/pingdom.server.controller.js
@@ -7,15 +7,31 @@ var CronJob = require('cron').CronJob,
 	pingdom = require('pingdom-api'),
 	keys	= require('../../config/keys/keys');
 
+var pingdomApi = require('pingdom-api')({
+    user: keys.pingdom.email,  // user account login
+    pass: keys.pingdom.password,  // user account password
+    appkey: keys.pingdom.apiKey
+});
+
+
 exports.init = function(){
 
 	var job = new CronJob({
 	  cronTime: '0 */1 * * * *',
 	  onTick: function() {
 	    console.log('Tick!');
+
+	    pingdomApi.getChecks()
+		.spread(function(checks, response){
+		  console.log(checks);
+		})
+		.catch(function(e){
+		  console.log(e);
+		});
+		console.log('Tack!');
 	  },
 	  start: false,
 	  timeZone: 'Europe/Amsterdam'
 	});
- 	// job.start();
+ 	job.start();
 };
