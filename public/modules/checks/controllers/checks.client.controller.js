5ea77837f665543f77390f3e84b0a6b89c27e852
Raoul de Best <raoul@aanzee.nl>
2015-07-02 23:57:51 +0200

    WIP: display data on view sent with socketio event

--- public/modules/checks/controllers/checks.client.controller.js
+++ public/modules/checks/controllers/checks.client.controller.js
@@ -3,9 +3,15 @@
 angular.module('checks').controller('ChecksController', ['$scope', 'Socket',
 	function($scope, Socket) {
 
-		Socket.on('checks.updated', function(checks) {
-    		console.log(checks);
+		Socket.on('checks.updated', function(data) {
+    		console.log(data);
+    		$scope.checks = data.checks;
+    		$scope.lastUpdatedAt(data.lastUpdate);
+    		console.log(data.checks);
 		});
 
+		$scope.lastUpdatedAt = function(timestamp){
+			$scope.time = timestamp;
+		}
 	}
 ]);
