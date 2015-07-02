9bd8dac64d67d9dd5d729ca8bd0f900635c8fe73
Raoul de Best <raoul@aanzee.nl>
2015-07-02 23:21:39 +0200

    Add socket to checks controller

--- public/modules/checks/controllers/checks.client.controller.js
+++ public/modules/checks/controllers/checks.client.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('checks').controller('ChecksController', ['$scope',
-	function($scope) {
-		// Controller Logic
-		// ...
+angular.module('checks').controller('ChecksController', ['$scope', 'Socket',
+	function($scope, Socket) {
+
+		Socket.on('checks.updated', function(checks) {
+    		console.log(checks);
+		});
+
 	}
 ]);
