31a78c1ceb1cccfd85cc7b18ee95ec048b0989f2
Raoul de Best <raoul@aanzee.nl>
2015-07-08 00:59:28 +0200

    Load last saved checks on page load

--- public/modules/checks/controllers/checks.client.controller.js
+++ public/modules/checks/controllers/checks.client.controller.js
@@ -1,17 +1,34 @@
 'use strict';
 
-angular.module('checks').controller('ChecksController', ['$scope', 'Socket',
-	function($scope, Socket) {
+angular.module('checks').controller('ChecksController', ['$scope', '$http', 'Socket',
+	function($scope, $http, Socket) {
+
+		this.init = function(){
+			this.fetchChecks();
+		};
+
+		this.fetchChecks = function(){
+			$http.get('/api').
+			success(function(data, status, headers, config) {
+				$scope.lastUpdatedAt(data.lastUpdate);
+    			$scope.checks = data.checks;
+			}).
+			error(function(data, status, headers, config) {
+				console.error('Error! Something went wrong');
+				console.error(data);
+			});
+		};
 
 		Socket.on('checks.updated', function(data) {
-    		console.log(data);
     		$scope.checks = data.checks;
     		$scope.lastUpdatedAt(data.lastUpdate);
-    		console.log(data.checks);
+    		console.log('>>> Updated the checks')
 		});
 
 		$scope.lastUpdatedAt = function(timestamp){
 			$scope.time = timestamp;
 		}
+
+		this.init();
 	}
 ]);
